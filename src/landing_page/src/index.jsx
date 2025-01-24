591108883a8ccc8537e6aa615f8b212c0b4afcc3
Jesse Williams <jessewilliams@MacBookPro.attlocal.net>
2025-01-24 11:03:04 -0600

    sns_init.yaml file added

--- src/landing_page/src/index.jsx
+++ src/landing_page/src/index.jsx
@@ -2,13 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Router from "./Router";
 import "./index.scss";
-import {  HashRouter } from "react-router-dom";
 
 
 ReactDOM.render(
-  <HashRouter>
-      <Router />
-  </HashRouter>,
-
+  <Router />,
   document.getElementById("root")
 );
